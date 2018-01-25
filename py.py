768ca22d9ef68e513b32c3d88bfefd2f8657f67a
fralychen <fralychem@gmail.com>
2018-01-25 21:20:28 +0800

    add Study_Project

---
+++ py.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+mintues = int(input("Enter mintues: "))
+def Hours(b):
+	n=b / 60
+	m=b % 60
+	print("%d H,%d M" %(n,m))
+if mintues < 0:
+	try:
+		raise ValueError
+	except ValueError:
+		print("this is negative number.")
+else:
+	Hours(mintues)
+new insert
